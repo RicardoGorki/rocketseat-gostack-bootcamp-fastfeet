36e06dbbc4e54074f917771635f356d886c0b723
RicardoGorki <ricardo.gorki@gmail.com>
2020-01-31 17:20:13 -0300

    recipientcontroller, 1/4 done

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -21,15 +21,55 @@ class RecipientController {
   }
 
   async store(req, res) {
-    return res.json({ error: 'error' });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      postcode: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipients = await Recipient.create(req.body);
+
+    return res.json({ recipients });
   }
 
   async update(req, res) {
-    return res.json({ error: 'error' });
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      postcode: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipients = await Recipient.findByPk(req.params.id);
+
+    await recipients.update(req.body);
+
+    return res.json({ recipients });
   }
 
   async delete(req, res) {
-    return res.json({ error: 'error' });
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(401).json({ error: 'Recipient do not found.' });
+    }
+    recipient.destroy();
+    return res.json({ message: 'Recipient deleted' });
   }
 }
 
