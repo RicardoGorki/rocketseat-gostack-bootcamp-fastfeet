fb81b5291c48ef099d1a5b6eb006a067cbccd902
RicardoGorki <ricardo.gorki@gmail.com>
2020-03-02 12:28:38 -0300

    multer

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
@@ -23,6 +24,10 @@ class App {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
