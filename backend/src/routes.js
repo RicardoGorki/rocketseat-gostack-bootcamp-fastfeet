794920ce35bbfb3fde6659743343be9273728c04
ricardogorki <ricardo.gorki@gmail.com>
2020-03-26 20:51:44 -0300

    signature, deliveries

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,12 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
+import SignatureController from './app/controllers/SignatureController';
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliveryController from './app/controllers/DeliveryController';
+import DeliveriesController from './app/controllers/DeliveriesController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 import DeliveryProblemController from './app/controllers/DeliveryProblemController';
 
@@ -18,11 +20,11 @@ const upload = multer(multerConfig);
 routes.get('/users', UserController.index);
 routes.get('/recipients', RecipientController.index);
 routes.post('/sessions', SessionController.store);
-routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users/', UserController.update);
+routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 
 routes.post('/recipients', RecipientController.store);
@@ -36,17 +38,19 @@ routes.post('/delivery', DeliveryController.store);
 routes.put('/delivery/:id', DeliveryController.update);
 routes.delete('/delivery/:id', DeliveryController.delete);
 
+routes.put('/delivery/:id/signature', SignatureController.update);
+
+routes.get('/deliveryman/:id/deliveries', DeliveriesController.index);
+routes.put('/delivery/:id/deliveries', DeliveriesController.update);
+
 routes.get('/deliverymans', DeliverymanController.index);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
 
+routes.get('/delivery/problems', DeliveryProblemController.show);
 routes.get('/delivery/:id/problems', DeliveryProblemController.index);
 routes.post('/delivery/:id/problems', DeliveryProblemController.store);
-routes.put('/delivery/:id/problems', DeliveryProblemController.update);
-routes.delete(
-  '/delivery/:id/cancel-delivery',
-  DeliveryProblemController.delete
-);
+routes.put('/problem/:id/cancel-delivery', DeliveryProblemController.update);
 
 export default routes;
