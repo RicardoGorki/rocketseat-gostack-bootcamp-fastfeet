c51fe320bcb8ac5319476ad7cb1fa2730eefc39b
ricardogorki <ricardo.gorki@gmail.com>
2020-03-26 20:55:03 -0300

    remove mongo

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,20 +1,19 @@
 import Sequelize from 'sequelize';
-import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
 import Deliveryman from '../app/models/Deliveryman';
 import Delivery from '../app/models/Delivery';
+import DeliveryProblem from '../app/models/DeliveryProblem';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File, Deliveryman, Delivery];
+const models = [User, Recipient, File, Deliveryman, Delivery, DeliveryProblem];
 
 class Database {
   constructor() {
     this.init();
-    this.mongo();
   }
 
   init() {
@@ -23,14 +22,6 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
-  }
-
-  mongo() {
-    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useFindAndModify: true,
-      useUnifiedTopology: true,
-    });
   }
 }
 
